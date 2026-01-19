06d928b05b00da2fb7e571fc5e499a0cfce8823f
Boomika Nallasivam <knallasivam1981ed@gmail.com>
2026-01-19 21:57:51 +0530

    Add files via upload

---
+++ 546.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k, n, w;
+    cin >> k >> n >> w;
+
+    int total = k * (w * (w + 1) / 2);
+
+    if (total > n)
+        cout << total - n;
+    else
+        cout << 0;
+
+    return 0;
+}
