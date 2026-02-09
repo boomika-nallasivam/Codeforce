248f51e5f36dd10cbaa98d238262fd2ce478b2a0
Boomika Nallasivam <knallasivam1981ed@gmail.com>
2026-02-09 20:15:47 +0530

    Create 716A.cpp

---
+++ 716A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    long long c;
+    cin >> n >> c;
+
+    vector<long long> t(n);
+    for (int i = 0; i < n; i++) {
+        cin >> t[i];
+    }
+
+    int count = 1; 
+
+    for (int i = 1; i < n; i++) {
+        if (t[i] - t[i - 1] <= c) {
+            count++;
+        } else {
+            count = 1; 
+        }
+    }
+
+    cout << count << "\n";
+    return 0;
+}
