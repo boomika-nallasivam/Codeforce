6b8e581e1350b00c80b44981fdebb013530dab89
Boomika Nallasivam <knallasivam1981ed@gmail.com>
2026-03-23 16:56:32 +0530

    Create 746A.cpp

---
+++ 746A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int k = min(a, min(b / 2, c / 4));
+
+    cout << k * 7 << endl;
+
+    return 0;
+}
